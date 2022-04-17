946d2d1bdf98d05cfea4246560895e6c2035a7c6
MontereyLeeAnnJenica <leeannjenica5@gmail.com>
2022-04-18 01:53:07 +0800

    Song 4

---
+++ song4.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song3- Sanctuary</title>
+        <link rel="icon" type="image/x-icon" href="assets/icon.jpg" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+       <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"><a class="nav-link text-light" href="index.php">Art Waves Lyrics</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                   <ul class="navbar-nav ms-auto">
+                     
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-black bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Art Waves Lyrics</h1>
+                <p class="lead">Where words leave off, music begins.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo"<h2 class='text-primary'>Lovely</h2>";
+                        echo"<p class='lead'>Billie Eilish and Khalid</p>";
+
+                        echo"<p>Thought I found a way</p>";
+                        echo"<p>Thought I found a way out (found)</p>";
+                        echo"<p>But you never go away (never go away)</p>";
+                        echo"<p>So I guess I gotta stay now</p>";
+                        echo"<br/>";
+
+                        echo"<p>Oh, I hope someday I'll make it out of here</p>";
+                        echo"<p>Even if it takes all night or a hundred years</p>";
+                        echo"<p>Need a place to hide, but I can't find one near</p>";
+                        echo"<p>Wanna feel alive, outside I can't fight my fear</p>";
+                        echo"<br/>";
+
+                        echo"<p>Isn't it lovely? All alone</p>";
+                        echo"<p>Heart made of glass, my mind of stone</p>";
+                        echo"<p>'Tear me to pieces, skin to bone</p>";
+                        echo"<p>Hello, welcome home</p>";
+                        echo"<br/>";
+                        
+                        echo"<p>Walking out of time</p>";
+                        echo"<p>Looking for a better place (looking for a better place)</p>";
+                        echo"<p>Something's on my mind</p>";
+                        echo"<p>Always in my head space</p>";
+                        echo"<br/>";
+                       
+                        echo"<p>But I know someday I'll make it out of here</p>";
+                        echo"<p>Even if it takes all night or a hundred years</p>";
+                        echo"<p>Need a place to hide, but I can't find one near</p>";
+                        echo"<p>Wanna feel alive, outside I can't fight my fear</p>";
+                        echo"<br/>";
+
+                        echo"<p>Isn't it lovely? All alone</p>";
+                        echo"<p>Heart made of glass, my mind of stone</p>";
+                        echo"<p>'Tear me to pieces, skin to bone</p>";
+                        echo"<p>Hello, welcome home</p>";
+                        echo"<br/>";
+
+                        echo"<p>Whoa, yeah</p>";
+                        echo"<p>Yeah, ah</p>";
+                        echo"<p>Whoa, whoa</p>";
+                        echo"<p>Hello, welcome home</p>";
+                        echo"<br/>";
+
+                        ?>
+                        
+                        Back to main page <a href = "index.php">here!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Lee-Ann Jenica R. Monterey</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
