8181bf1797e4971b2ac886fd2c9059b7a5244679
MontereyLeeAnnJenica <leeannjenica5@gmail.com>
2022-04-18 01:32:00 +0800

    Song 2

---
+++ song2.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song1- 214</title>
+        <link rel="icon" type="image/x-icon" href="assets/icon.jpg" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+       <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"><a class="nav-link text-light" href="index.php">Art Waves Lyrics</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                   <ul class="navbar-nav ms-auto">
+                     
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-black bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Art Waves Lyrics</h1>
+                <p class="lead">Where words leave off, music begins.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo"<h2 class='text-primary'>You Never Know</h2>";
+                        echo"<p class='lead'>BLACKPINK</p>";
+
+                        echo"<p>On the day when I laughed hard</p>";
+                        echo"<p>Why is the night darker?</p>";
+                        echo"<p>It keeps bringing me down, down, down</p>";
+                        echo"<br/>";
+                       
+                       
+                        echo"<p>Hmm, the words that everyone said so easily</p>";
+                        echo"<p>Maybe you can hear it soon</p>";
+                        echo"<p>I've heard enough I've heard enough</p>";
+                        echo"<p>Of the things that I'm not</p>";
+                        echo"<br/>";
+
+                        echo"<p>Even if the whole world changes</p>";
+                        echo"<p>I'm still the same</p>";
+                        echo"<p>I dreamed of this path I walked</p>";
+                        echo"<p>As it was then, as it was then</p>";
+                        echo"<p>The first time I danced every day</p>";
+                        echo"<p>I was left there</p>";
+                        echo"<br/>";
+
+                        echo"<p>But you'll never know unless you walk in my shoes</p>";
+                        echo"<p>You'll never know, my tangled strings</p>";
+                        echo"<p>'Cause everybody sees what they wanna see</p>";
+                        echo"<p>'It's easier to judge me than to believe</p>";
+                        echo"<br/>";
+                        
+                        echo"<p>The old thoughts I hid deeply</p>";
+                        echo"<p>Sometimes grabbing me and tormenting me</p>";
+                        echo"<p>The more I do, I'mma shine baby</p>";
+                        echo"<p>You know they ain't got a shot on me</p>";
+                        echo"<br/>";
+                       
+                        echo"<p>But you'll never know unless you walk in my shoes</p>";
+                        echo"<p>You'll never know, my tangled strings</p>";
+                        echo"<p>'Cause everybody sees what they wanna see</p>";
+                        echo"<p>It's easier to judge me than to believe</p>";
+                        
+
+                        ?>
+                        
+                        Back to main page <a href = "index.php">here!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Lee-Ann Jenica R. Monterey</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
