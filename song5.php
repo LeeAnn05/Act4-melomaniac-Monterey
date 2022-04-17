3045ca59c195bdb9aa5a2af0b28d9ebf54cedbbd
MontereyLeeAnnJenica <leeannjenica5@gmail.com>
2022-04-18 02:17:31 +0800

    Song 5

---
+++ song5.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song5-Rainbow</title>
+        <link rel="icon" type="image/x-icon" href="assets/icon.jpg" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+       <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"><a class="nav-link text-light" href="index.php">Art Waves Lyrics</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                   <ul class="navbar-nav ms-auto">
+                     
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-black bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Art Waves Lyrics</h1>
+                <p class="lead">Where words leave off, music begins.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo"<h2 class='text-primary'>Rainbow</h2>";
+                        echo"<p class='lead'>Gatton</p>";
+
+                        echo"<p>When it rains, it pours</p>";
+                        echo"<p>But you didn't even notice</p>";
+                        echo"<p>It ain't rainin' anymore</p>";
+                        echo"<p>It's hard to breathe when all you know is</p>";
+                        echo"<p>The struggle of stayin' above the risin' water lines</p>";
+                        echo"<br/>";
+
+                        echo"<p>Well, the sky has finally opened</p>";
+                        echo"<p>The rain and wind stopped blowin'</p>";
+                        echo"<p>But you're stuck out in the same ol' storm again</p>";
+                        echo"<p>You hold tight to your umbrella</p>";
+                        echo"<p>Well, darlin', I'm just tryin' to tell ya</p>";
+                        echo"<p>That there's always been a rainbow</p>";
+                        echo"<p>Hangin' over your head</p>";
+                        echo"<br/>";
+
+                        echo"<p>If you could see what I see</p>";
+                        echo"<p>You'd be blinded by the colors</p>";
+                        echo"<p>Yellow, red, and orange, and green</p>";
+                        echo"<p>And at least a million others</p>";
+                        echo"<p>So tie up the boat, take off your coat</p>";
+                        echo"<p>And take a look around</p>";
+                        echo"<br/>";
+                        
+                         echo"<p>Well, the sky has finally opened</p>";
+                        echo"<p>The rain and wind stopped blowin'</p>";
+                        echo"<p>But you're stuck out in the same ol' storm again</p>";
+                        echo"<p>You hold tight to your umbrella</p>";
+                        echo"<p>Well, darlin', I'm just tryin' to tell ya</p>";
+                        echo"<p>That there's always been a rainbow</p>";
+                        echo"<p>Hangin' over your head</p>";
+                        echo"<br/>";
+                       
+                        echo"<pOh, tie up the boat, take off your coat</p>";
+                        echo"<p>And take a look around</p>";
+                        echo"<p>Everything is alright now</p>";
+                        echo"<br/>";
+
+                        echo"<p>Well, the sky has finally opened</p>";
+                        echo"<p>The rain and wind stopped blowin'</p>";
+                        echo"<p>But you're stuck out in the same ol' storm again</p>";
+                        echo"<p>You hold tight to your umbrella</p>";
+                        echo"<p>Well, darlin', I'm just tryin' to tell ya</p>";
+                        echo"<p>That there's always been a rainbow</p>";
+                        echo"<p>Hangin' over your head</p>";
+                        echo"<p>Yeah, there's always been a rainbow</p>";
+                        echo"<p>Hangin' over</p>";
+                        echo"<p>Your head</p>";
+                        echo"<br/>";
+
+                        ?>
+                        
+                        Back to main page <a href = "index.php">here!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Lee-Ann Jenica R. Monterey</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
