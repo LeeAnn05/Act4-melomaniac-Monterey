6f75dbacd7ad234ff47bcc7475e63affea4008b3
MontereyLeeAnnJenica <leeannjenica5@gmail.com>
2022-04-18 01:43:04 +0800

    Song 3

---
+++ song3.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song3- Sanctuary</title>
+        <link rel="icon" type="image/x-icon" href="assets/icon.jpg" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+       <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"><a class="nav-link text-light" href="index.php">Art Waves Lyrics</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                   <ul class="navbar-nav ms-auto">
+                     
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-black bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Art Waves Lyrics</h1>
+                <p class="lead">Where words leave off, music begins.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo"<h2 class='text-primary'>Sanctuary</h2>";
+                        echo"<p class='lead'>Joji</p>";
+
+                        echo"<p>Go ahead and bark after dark, fallen star</p>";
+                        echo"<p>I'm your one-call away</p>";
+                        echo"<p>Motel halls, neon walls</p>";
+                        echo"<p>When night falls I am your escape</p>";
+                        echo"<p>When you lay alone, I ache</p>";
+                        echo"<p>Something I wanted to feel</p>";
+                        echo"<br/>";
+
+                        echo"<p>If you've been waiting for falling in love</p>";
+                        echo"<p>Babe, you don't have to wait on me</p>";
+                        echo"<p>'Cause I've been aiming for heaven above</p>";
+                        echo"<p>But an angel ain't what I need</p>";
+                        echo"<br/>";
+
+                        echo"<p>Not anyone, you're the one</p>";
+                        echo"<p>More than fun, you're the sanctuary</p>";
+                        echo"<p>'Cause what you want is what I want, sincerity</p>";
+                        echo"<br/>";
+                        
+                        echo"<p>Souls that dream alone lie awake</p>";
+                        echo"<p>I'll give you something so real</p>";
+                        echo"<br/>";
+                       
+                        echo"<p>If you've been waiting for falling in love</p>";
+                        echo"<p>Babe, you don't have to wait on me</p>";
+                        echo"<p>'Cause I've been aiming for heaven above</p>";
+                        echo"<p>But an angel ain't what I need</p>";
+                        echo"<br/>";
+
+                        echo"<p>Hold me oh so close, 'cause you never know</p>";
+                        echo"<p>Just how long our lives will be</p>";
+                        echo"<br/>";
+
+                        echo"<p>If you've been waiting for falling in love</p>";
+                        echo"<p>Babe, you don't have to wait on me</p>";
+                        echo"<p>'Cause I've been aiming for heaven above</p>";
+                        echo"<p>But an angel ain't what I need</p>";
+                        echo"<br/>";
+
+                        ?>
+                        
+                        Back to main page <a href = "index.php">here!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Lee-Ann Jenica R. Monterey</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
